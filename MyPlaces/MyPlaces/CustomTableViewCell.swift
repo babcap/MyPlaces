dcb10906569a7d05fa0090ef8e2404401f3d13c9
Arthur <takerarthur@gmail.com>
2020-04-02 12:06:11 +0300

    App Icon Custom table view cell

---
+++ MyPlaces/MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Arthur Batyaev on 4/2/20.
+//  Copyright © 2020 Arthur Batyaev. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
