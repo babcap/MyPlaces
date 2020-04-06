f4377203023235e55595af3b7323fe6e8c2b4d73
Arthur <takerarthur@gmail.com>
2020-04-06 14:10:12 +0300

    Import realm

--- MyPlaces/MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/MyPlaces/NewPlaceViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
     
-    var newPlace: Place?
+    var newPlace = Place()
     var imageIsChanged = false
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
@@ -22,6 +22,11 @@ class NewPlaceViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        DispatchQueue.main.async {
+            self.newPlace.savePlaces()
+        }
+       
         
         tableView.tableFooterView = UIView()
 
@@ -77,12 +82,12 @@ class NewPlaceViewController: UITableViewController {
             image = #imageLiteral(resourceName: "imagePlaceholder")
         }
         
-        newPlace = Place(name: placeName.text!,
-                         location: placeLocation.text,
-                         type: placeType.text,
-                         image: image,
-                         restaurantImage: nil)
-        
+//        newPlace = Place(name: placeName.text!,
+//                         location: placeLocation.text,
+//                         type: placeType.text,
+//                         image: image,
+//                         restaurantImage: nil)
+//
     }
     
     @IBAction func cancelAction(_ sender: Any) {
