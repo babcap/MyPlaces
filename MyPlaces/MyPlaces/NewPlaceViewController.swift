bcd94bf0600492bc952ab06784aa576ce9f8da74
Arthur <takerarthur@gmail.com>
2020-04-06 15:39:34 +0300

    Saving object to DB

--- MyPlaces/MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/MyPlaces/NewPlaceViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
     
-    var newPlace = Place()
     var imageIsChanged = false
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
@@ -23,10 +22,6 @@ class NewPlaceViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        DispatchQueue.main.async {
-            self.newPlace.savePlaces()
-        }
-       
         
         tableView.tableFooterView = UIView()
 
@@ -74,6 +69,8 @@ class NewPlaceViewController: UITableViewController {
     
     func saveNewPlace() {
         
+        
+        
         var image: UIImage?
         
         if imageIsChanged {
@@ -82,12 +79,14 @@ class NewPlaceViewController: UITableViewController {
             image = #imageLiteral(resourceName: "imagePlaceholder")
         }
         
-//        newPlace = Place(name: placeName.text!,
-//                         location: placeLocation.text,
-//                         type: placeType.text,
-//                         image: image,
-//                         restaurantImage: nil)
-//
+        let imageData = image?.pngData()
+        
+        let newPlace = Place(name: placeName.text!,
+                             location: placeLocation.text,
+                             type: placeType.text,
+                             imageData: imageData)
+        
+        StorageManager.saveObject(newPlace)
     }
     
     @IBAction func cancelAction(_ sender: Any) {
