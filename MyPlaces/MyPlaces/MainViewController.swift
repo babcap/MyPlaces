be41016b4817c534525ac9ca33c5e9c34682f2b6
Arthur <takerarthur@gmail.com>
2020-04-07 09:51:41 +0300

    Sorting

--- MyPlaces/MyPlaces/MainViewController.swift
+++ MyPlaces/MyPlaces/MainViewController.swift
@@ -11,10 +11,13 @@ import RealmSwift
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var reversedSortingButton: UIBarButtonItem!
     
-     var places: Results<Place>!
+    
+    var places: Results<Place>!
+    var ascendingSorting = true
     
     
     override func viewDidLoad() {
@@ -22,30 +25,30 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
         places = realm.objects(Place.self)
     }
-
+    
     // MARK: - Table view data source
-
-
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-
+        
         let place = places[indexPath.row]
-
+        
         cell.nameLabel.text = place.name
         cell.locationLable.text = place.location
         cell.typeLabel.text = place.type
         cell.imageOfPlace.image = UIImage(data: place.imageData!)
-
+        
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
-
+        
         return cell
     }
-
+    
     // MARK: - Table View Delegate
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
@@ -59,10 +62,10 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         DeleteAction.backgroundColor = .red
         return UISwipeActionsConfiguration(actions: [DeleteAction])
     }
-   
-
+    
+    
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
@@ -71,14 +74,41 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
             newPlaceVC.currentPlace = place
         }
     }
-
-
+    
+    
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
-         
+        
         guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
         
         newPlaceVC.savePlace()
         tableView.reloadData()
         
+    }
+    
+    @IBAction func sortSelection(_ sender: UISegmentedControl) {
+        
+        sorting()
+    }
+    @IBAction func reversedSorting(_ sender: Any) {
+        
+        ascendingSorting.toggle()
+        
+        if ascendingSorting {
+            reversedSortingButton.image = #imageLiteral(resourceName: "AZ")
+        } else {
+            reversedSortingButton.image = #imageLiteral(resourceName: "ZA")
+        }
+        
+        sorting()
+    }
+    
+    private func sorting() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            places = places.sorted(byKeyPath: "date", ascending: ascendingSorting)
+        } else {
+            places = places.sorted(byKeyPath: "name", ascending: ascendingSorting)
+        }
+        
+        tableView.reloadData()
     }
 }
